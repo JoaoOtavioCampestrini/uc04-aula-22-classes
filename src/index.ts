bef8f5917f4e61e8ec05d10840b0fb21baa73c3f
Joao Otavio <joaootaviocampestrini@gmail.com>
2025-01-23 19:17:46 -0300

    primeiro commit

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+// classe que representa o item de uma lista de tarefas
+
+class tarefa {
+  nome: string;
+  concluida: boolean;
+}
+
+class tarefa {
+
+  constructor(nome: string) {
+    nome = 
+  }
+
+  nome: string;
+  concluida: boolean;
+
+
+  public get nome (): string {
+    return this._nome;
+  }
+
+  public set nome (nome: string) {
+    this._nome = nome;
+  }
+
+
+
+  public set concluida (concluida: boolean) {
+    this._concluida = concluida;
+  }
+}
+
+const tarefa 1 = new tarefa("Aprender JavaScript");
